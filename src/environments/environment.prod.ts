820704e88fb6d3e67bdc11cd1bc325654821434f
criaturitavalu <youtics@gmail.com>
2023-01-26 10:45:56 -0300

    Version final tarjeta de credito con firestore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB_xMaRylUoEY7HXa-WjrzKWY76NcGMRQI',
+    authDomain: 'tarjeta-7c80b.firebaseapp.com',
+    databaseURL: '<your-database-URL>',
+    projectId: 'tarjeta-7c80b',
+    storageBucket: 'tarjeta-7c80b.appspot.com',
+    messagingSenderId: '425253856876',
+    appId: '1:425253856876:web:6858913b24fbf0db9a1a49',
+  }
+  
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB_xMaRylUoEY7HXa-WjrzKWY76NcGMRQI',
+    authDomain: 'tarjeta-7c80b.firebaseapp.com',
+    databaseURL: '<your-database-URL>',
+    projectId: 'tarjeta-7c80b',
+    storageBucket: 'tarjeta-7c80b.appspot.com',
+    messagingSenderId: '425253856876',
+    appId: '1:425253856876:web:6858913b24fbf0db9a1a49',
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
